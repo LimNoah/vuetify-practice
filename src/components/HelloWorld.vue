1fd9e093ae94bac614302748caf512ba13094294
LimNoah <limnoah0301@gmail.com>
2022-12-22 09:23:05 +0900

    refactor: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,11 +29,36 @@
         class="mb-5"
         cols="12"
       >
-        <v-alert
-        dense
-        type="success">
-        I'm an Alert Usage Example
-      </v-alert>
+        <div>
+          <v-alert
+            border="top"
+            color="red lighten-2"
+            dark
+          >
+            I'm an alert with a top border and red color
+          </v-alert>
+          <v-alert
+            border="right"
+            color="blue-grey"
+            dark
+          >
+            I'm an alert with a right border and blue-grey color
+          </v-alert>
+          <v-alert
+            border="bottom"
+            color="pink darken-1"
+            dark
+          >
+            I'm an alert with a bottom border and pink color
+          </v-alert>
+          <v-alert
+            border="left"
+            color="indigo"
+            dark
+          >
+            I'm an alert with a border left type info
+          </v-alert>
+        </div>
       </v-col>
 
       <v-col
