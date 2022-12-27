e4a24a788034d466a6fecdbf0bd122e6e11b4c8a
LimNoah <limnoah0301@gmail.com>
2022-12-27 11:10:33 +0900

    refactor: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -159,6 +159,30 @@
                 >
                   Praesent congue erat at massa. Nullam vel sem. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. Curabitur at lacus ac velit ornare lobortis.
                 </v-alert>
+                <v-alert
+                  outlined
+                  color="purple"
+                >
+                  <div class="text-h6">
+                    Lorem Ipsum
+                  </div>
+                  <div>Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Duis vel nibh at velit scelerisque suscipit. Praesent blandit laoreet nibh. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros.</div>
+                </v-alert>
+                <v-alert
+                  outlined
+                  type="success"
+                  text
+                >
+                  Praesent venenatis metus at tortor pulvinar varius. Aenean commodo ligula eget dolor. Praesent ac massa at ligula laoreet iaculis. Vestibulum ullamcorper mauris at ligula.
+                </v-alert>
+                <v-alert
+                  outlined
+                  type="warning"
+                  prominent
+                  border="left"
+                >
+                  Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Suspendisse non nisl sit amet velit hendrerit rutrum. Nullam vel sem. Pellentesque dapibus hendrerit tortor.
+                </v-alert>
               <div class="text-center">
                 <v-btn
                   v-if="!alert"
