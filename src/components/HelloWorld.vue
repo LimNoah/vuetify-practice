4b5fe9e5e9fb2e21acdb6ae2cbd5332e67b6bf96
LimNoah <limnoah0301@gmail.com>
2022-12-29 13:39:36 +0900

    refactor: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -213,6 +213,21 @@
                 >
                   Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Sed in libero ut nibh placerat accumsan.. Curabitur blandit mollis lacus. Curabitur blandit mollis lacus.
                 </v-alert>
+                <v-alert type="success">
+                  I'm a success alert.
+                </v-alert>
+
+                <v-alert type="info">
+                  I'm an info alert.
+                </v-alert>
+
+                <v-alert type="warning">
+                  I'm a warning alert.
+                </v-alert>
+
+                <v-alert type="error">
+                  I'm an error alert.
+                </v-alert>
               <div class="text-center">
                 <v-btn
                   v-if="!alert"
