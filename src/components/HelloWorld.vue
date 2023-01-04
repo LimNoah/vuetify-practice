45ebf86bfcf0992cea4a922c38bfe11d0cfedbe9
LimNoah <limnoah0301@gmail.com>
2023-01-04 10:34:35 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -390,6 +390,36 @@
             <v-responsive height="1500"></v-responsive>
           </v-sheet>
         </v-card>
+        <v-bottom-navigation
+          v-model="value"
+          :background-color="color"
+          dark
+          shift
+        >
+          <v-btn>
+            <span>Video</span>
+
+            <v-icon>mdi-television-play</v-icon>
+          </v-btn>
+
+          <v-btn>
+            <span>Music</span>
+
+            <v-icon>mdi-music-note</v-icon>
+          </v-btn>
+
+          <v-btn>
+            <span>Book</span>
+
+            <v-icon>mdi-book</v-icon>
+          </v-btn>
+
+          <v-btn>
+            <span>Image</span>
+
+            <v-icon>mdi-image</v-icon>
+          </v-btn>
+        </v-bottom-navigation>
       </v-col>
       
       <v-col
