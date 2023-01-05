cf003ae4e93f2eb3a34f7d312450b6cbe072ad33
LimNoah <limnoah0301@gmail.com>
2023-01-05 14:14:19 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -420,6 +420,41 @@
             <v-icon>mdi-image</v-icon>
           </v-btn>
         </v-bottom-navigation>
+        <div class="overflow-hidden">
+          <div class="text-center mb-8">
+            <v-btn
+              color="deep-purple"
+              outlined
+              @click="active = !active"
+            >
+              Toggle Navigation
+            </v-btn>
+          </div>
+
+          <v-bottom-navigation
+            v-model="value"
+            :input-value="active"
+            color="indigo"
+          >
+            <v-btn>
+              <span>Recents</span>
+
+              <v-icon>mdi-history</v-icon>
+            </v-btn>
+
+            <v-btn>
+              <span>Favorites</span>
+
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn>
+              <span>Nearby</span>
+
+              <v-icon>mdi-map-marker</v-icon>
+            </v-btn>
+          </v-bottom-navigation>
+        </div>
       </v-col>
       
       <v-col
