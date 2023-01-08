972fc6d667accc35aef2e64ed50793a37c1816a1
LimNoah <limnoah0301@gmail.com>
2023-01-08 13:27:53 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -487,30 +487,63 @@
           </v-sheet>
         </v-bottom-sheet>
         <v-btn
-      color="blue"
-      dark
-      @click="sheet = !sheet"
-    >
-      Open v-model
-    </v-btn>
-    <v-bottom-sheet v-model="sheet">
-      <v-sheet
-        class="text-center"
-        height="200px"
-      >
-        <v-btn
-          class="mt-6"
-          text
-          color="red"
+          color="blue"
+          dark
           @click="sheet = !sheet"
         >
-          close
+          Open v-model
         </v-btn>
-        <div class="py-3">
-          This is a bottom sheet using the controlled by v-model instead of activator
+        <v-bottom-sheet v-model="sheet">
+          <v-sheet
+            class="text-center"
+            height="200px"
+          >
+            <v-btn
+              class="mt-6"
+              text
+              color="red"
+              @click="sheet = !sheet"
+            >
+              close
+            </v-btn>
+            <div class="py-3">
+              This is a bottom sheet using the controlled by v-model instead of activator
+            </div>
+          </v-sheet>
+        </v-bottom-sheet>
+        <div class="text-center">
+          <v-bottom-sheet
+            v-model="sheet"
+            persistent
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="green"
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                Open Persistent
+              </v-btn>
+            </template>
+            <v-sheet
+              class="text-center"
+              height="200px"
+            >
+              <v-btn
+                class="mt-6"
+                text
+                color="error"
+                @click="sheet = !sheet"
+              >
+                close
+              </v-btn>
+              <div class="py-3">
+                This is a bottom sheet using the persistent prop
+              </div>
+            </v-sheet>
+          </v-bottom-sheet>
         </div>
-      </v-sheet>
-    </v-bottom-sheet>
       </v-col>
       
       <v-col
