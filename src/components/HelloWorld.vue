eebb6017cdf3a82d908322c575b2cc10763a56a0
LimNoah <limnoah0301@gmail.com>
2023-01-12 13:27:21 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -597,6 +597,41 @@
             </v-card>
           </v-bottom-sheet>
         </div>
+        <div class="text-center">
+          <v-bottom-sheet v-model="sheet">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="purple"
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                Open In
+              </v-btn>
+            </template>
+            <v-list>
+              <v-subheader>Open in</v-subheader>
+              <v-list-item
+                v-for="tile in tiles"
+                :key="tile.title"
+                @click="sheet = false"
+              >
+                <v-list-item-avatar>
+                  <v-avatar
+                    size="32px"
+                    tile
+                  >
+                    <img
+                      :src="`https://cdn.vuetifyjs.com/images/bottom-sheets/${tile.img}`"
+                      :alt="tile.title"
+                    >
+                  </v-avatar>
+                </v-list-item-avatar>
+                <v-list-item-title>{{ tile.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-bottom-sheet>
+        </div>
       </v-col>
       
       <v-col
