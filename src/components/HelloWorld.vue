40b3d010023520e5994c8a7d89862c2eda32a51a
LimNoah <limnoah0301@gmail.com>
2023-01-17 09:35:09 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -836,6 +836,17 @@
         Reset Validation
       </v-btn>
     </v-form>
+    <v-row>
+      <v-col cols="12">
+        <v-input
+          :messages="['Messages']"
+          append-icon="mdi-close"
+          prepend-icon="mdi-phone"
+        >
+          Default Slot
+        </v-input>
+      </v-col>
+    </v-row>
   </v-container>
 
 </template>
