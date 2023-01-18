691b7063679d2c5acaed78bb1beff9eaea7e4167
LimNoah <limnoah0301@gmail.com>
2023-01-18 15:18:48 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -847,6 +847,14 @@
         </v-input>
       </v-col>
     </v-row>
+    <div>
+      <v-text-field
+        label="Main input"
+        :rules="rules"
+        hide-details="auto"
+      ></v-text-field>
+      <v-text-field label="Another input"></v-text-field>
+    </div>
   </v-container>
 
 </template>
