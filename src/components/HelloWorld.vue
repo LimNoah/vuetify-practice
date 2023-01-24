94af209b41f1b7ca5aa435bd4c5f30386693df9e
LimNoah <limnoah0301@gmail.com>
2023-01-25 00:53:48 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -943,6 +943,77 @@
         ></v-calendar>
       </v-sheet>
     </div>
+    <v-row class="fill-height">
+      <v-col>
+        <v-sheet height="64">
+          <v-toolbar
+            flat
+          >
+            <v-btn
+              outlined
+              class="mr-4"
+              color="grey darken-2"
+              @click="setToday"
+            >
+              Today
+            </v-btn>
+            <v-btn
+              fab
+              text
+              small
+              color="grey darken-2"
+              @click="prev"
+            >
+              <v-icon small>
+                mdi-chevron-left
+              </v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              text
+              small
+              color="grey darken-2"
+              @click="next"
+            >
+              <v-icon small>
+                mdi-chevron-right
+              </v-icon>
+            </v-btn>
+            <v-toolbar-title v-if="$refs.calendar">
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </v-sheet>
+        <v-sheet height="600">
+          <v-calendar
+            ref="calendar"
+            v-model="focus"
+            color="primary"
+            type="category"
+            category-show-all
+            :categories="categories"
+            :events="events"
+            :event-color="getEventColor"
+            @change="fetchEvents"
+          ></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <!-- <v-row>
+      <v-col>
+        <v-sheet height="400">
+          <v-calendar
+            ref="calendar"
+            :now="today"
+            :value="today"
+            :events="events"
+            color="primary"
+            type="week"
+          ></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row> -->
 
   </v-container>
 
