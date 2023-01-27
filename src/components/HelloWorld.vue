ecb0a0ee9c9dd75d72dfce1acc39aa79be78c59e
LimNoah <limnoah0301@gmail.com>
2023-01-27 09:31:44 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1014,6 +1014,35 @@
         </v-sheet>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <v-sheet height="500">
+          <v-calendar
+            :now="today"
+            :value="today"
+            color="primary"
+          >
+            <template v-slot:day="{ past, date }">
+              <v-row
+                class="fill-height"
+              >
+                <template v-if="past && tracked[date]">
+                  <v-sheet
+                    v-for="(percent, i) in tracked[date]"
+                    :key="i"
+                    :title="category[i]"
+                    :color="colors[i]"
+                    :width="`${percent}%`"
+                    height="100%"
+                    tile
+                  ></v-sheet>
+                </template>
+              </v-row>
+            </template>
+          </v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
 
   </v-container>
 
