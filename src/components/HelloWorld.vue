2dc5edebcb1faef6374046a1c738c8a476b8d828
LimNoah <limnoah0301@gmail.com>
2023-02-16 21:47:36 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1757,6 +1757,22 @@
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
+    <v-carousel>
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+      ></v-carousel-item>
+
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+        cover
+      ></v-carousel-item>
+
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        cover
+      ></v-carousel-item>
+    </v-carousel>
 
   </v-container>
 
