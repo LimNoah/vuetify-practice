fd9f1ea7759c3c2e1c6c6df1973adc9563956c48
LimNoah <limnoah0301@gmail.com>
2023-02-18 10:26:14 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1803,6 +1803,26 @@
         </v-carousel-item>
       </v-carousel>
     </v-card>
+    <div class="text-center">
+      <v-chip
+        v-if="chip"
+        class="ma-2"
+        closable
+        @click:close="chip = false"
+      >
+        Closable
+      </v-chip>
+
+      <v-btn
+        v-if="!chip"
+        close
+        color="primary"
+        dark
+        @click="chip = true"
+      >
+        Reset Chip
+      </v-btn>
+    </div>
 
   </v-container>
 
