e2bafbda414938f209a8c9879d82c61d84203655
LimNoah <limnoah0301@gmail.com>
2023-02-20 23:51:54 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1823,6 +1823,43 @@
         Reset Chip
       </v-btn>
     </div>
+    <div class="text-center">
+      <v-chip
+        class="ma-2"
+      >
+        Default
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="primary"
+      >
+        Primary
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="secondary"
+      >
+        Secondary
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="red"
+        text-color="white"
+      >
+        Red Chip
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="green"
+        text-color="white"
+      >
+        Green Chip
+      </v-chip>
+    </div>
 
   </v-container>
 
