c13e8f5dab57f58001fa9fdb08b1b42af307d2b7
LimNoah <limnoah0301@gmail.com>
2023-02-22 20:53:49 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1868,6 +1868,43 @@
         Default
       </v-chip>
     </v-row>
+    <div class="text-center">
+      <v-chip
+        class="ma-2"
+        label
+      >
+        Label
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="pink"
+        label
+        text-color="white"
+      >
+        <v-icon start icon="mdi-label"></v-icon>
+        Tags
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        color="primary"
+        label
+      >
+        <v-icon start icon="mdi-account-circle-outline"></v-icon>
+        John Leider
+      </v-chip>
+
+      <v-chip
+        class="ma-2"
+        closable
+        color="cyan"
+        label
+      >
+        <v-icon start icon="mdi-twitter"></v-icon>
+        New Tweets
+      </v-chip>
+    </div>
 
   </v-container>
 
