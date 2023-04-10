31331bf5e0b72a323e9095da673ce419149978b0
LimNoah <limnoah0301@gmail.com>
2023-04-10 09:30:09 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3291,6 +3291,25 @@
         </template>
       </v-banner>
     </div>
+    <v-banner
+      lines="one"
+      icon="mdi-wifi-strength-alert-outline"
+      color="warning"
+    >
+      <template v-slot:text>
+        No Internet connection
+      </template>
+
+      <template v-slot:actions>
+        <v-btn>
+          Dismiss
+        </v-btn>
+
+        <v-btn>
+          Retry
+        </v-btn>
+      </template>
+    </v-banner>
 
   </v-container>
 
