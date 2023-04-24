8681d629fc31ed1c9dbfcaef5a2559c9b9d14938
LimNoah <limnoah0301@gmail.com>
2023-04-25 08:57:20 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3552,6 +3552,30 @@
         </v-icon>
       </v-avatar>
     </div>
+    <div class="text-center ma-2">
+      <v-btn
+        dark
+        @click="snackbar = true"
+      >
+        Open Snackbar
+      </v-btn>
+      <v-snackbar
+        v-model="snackbar"
+      >
+        {{ text }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="snackbar = false"
+          >
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </div>
   </v-container>
 
 </template>
