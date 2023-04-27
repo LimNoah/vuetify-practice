b84fd38b66414b61afa8e69252836c7a0ca9af12
LimNoah <limnoah0301@gmail.com>
2023-04-27 13:42:22 +0900

    refactor: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3603,6 +3603,33 @@
         </template>
       </v-snackbar>
     </div>
+    <div class="text-center">
+      <v-btn
+        dark
+        color="indigo"
+        @click="snackbar = true"
+      >
+        Open Snackbar
+      </v-btn>
+
+      <v-snackbar
+        v-model="snackbar"
+        :vertical="vertical"
+      >
+        {{ text }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn
+            color="indigo"
+            text
+            v-bind="attrs"
+            @click="snackbar = false"
+          >
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </div>
   </v-container>
 
 </template>
