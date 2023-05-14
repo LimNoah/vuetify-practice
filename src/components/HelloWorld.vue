b2682cf9ad8795e57ec23dadfaba4a804deb8a7b
LimNoah <limnoah0301@gmail.com>
2023-05-15 00:19:19 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4130,6 +4130,61 @@
         </v-stepper-step>
       </v-stepper-header>
     </v-stepper>
+    <div>
+      <v-card class="mb-4">
+        <v-card-text>
+          <v-select
+            v-model="steps"
+            :items="[2, 3, 4, 5, 6]"
+            label="# of steps"
+          ></v-select>
+        </v-card-text>
+      </v-card>
+      <v-stepper v-model="e1">
+        <v-stepper-header>
+          <template v-for="n in steps">
+            <v-stepper-step
+              :key="`${n}-step`"
+              :complete="e1 > n"
+              :step="n"
+              editable
+            >
+              Step {{ n }}
+            </v-stepper-step>
+
+            <v-divider
+              v-if="n !== steps"
+              :key="n"
+            ></v-divider>
+          </template>
+        </v-stepper-header>
+
+        <v-stepper-items>
+          <v-stepper-content
+            v-for="n in steps"
+            :key="`${n}-content`"
+            :step="n"
+          >
+            <v-card
+              class="mb-12"
+              color="grey lighten-1"
+              height="200px"
+            ></v-card>
+
+            <v-btn
+              color="primary"
+              @click="nextStep(n)"
+            >
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </div>
   </v-container>
 </template>
 
