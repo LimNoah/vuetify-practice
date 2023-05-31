930c65d9aac5e9447edb65067600126e62947714
LimNoah <limnoah0301@gmail.com>
2023-05-31 09:03:02 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4246,6 +4246,11 @@
         class="ma-2"
         density="comfortable"
       ></v-rating>
+      <v-rating
+        v-model="rating"
+        class="ma-2"
+        density="compact"
+      ></v-rating>
     </div>
   </v-container>
 </template>
