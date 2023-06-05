8667c5c2c9c9f12008ace64bbc55aa69dd18d3bc
LimNoah <limnoah0301@gmail.com>
2023-06-05 20:15:50 +0900

    feat: Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4252,6 +4252,43 @@
         density="compact"
       ></v-rating>
     </div>
+    <div class="text-center">
+      <v-rating
+        v-model="rating"
+        clearable
+      ></v-rating>
+    </div>
+    <div class="d-flex flex-column align-center">
+      <v-rating
+        model-value="3"
+        size="x-small"
+      ></v-rating>
+
+      <v-rating
+        model-value="3"
+        size="small"
+      ></v-rating>
+
+      <v-rating
+        model-value="3"
+      ></v-rating>
+
+      <v-rating
+        model-value="3"
+        size="large"
+      ></v-rating>
+
+      <v-rating
+        model-value="3"
+        size="x-large"
+      ></v-rating>
+
+      <v-rating
+        model-value="3"
+        size="72"
+      ></v-rating>
+    </div>
+
   </v-container>
 </template>
 
